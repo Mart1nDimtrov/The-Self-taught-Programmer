da5fc4f2432b8ce51beb4c195f6db69e1adc6eaf
giggly <giggly@abv.bg>
2018-05-01 19:33:17 +0300

    OOP principles

---
+++ 10. Object-oriented Programming/inheritance_example.py
@@ -0,0 +1,19 @@
+class Adult():
+	def __init__(self, name, height, weight, eye_color):
+		# height is in feet, weight in lbs
+		self.name = name
+		self.height = height
+		self.weight = weight
+		self.eye_color = eye_color
+
+	def print_name(self):
+		print(self.name)
+
+class Kid(Adult):
+	def print_cartoon(self, favorite_cartoon):
+		print("{}'s favorite cartoon is {}".format(self.name, favorite_cartoon))
+
+
+child = Kid("Lauren", 3, 50, "blue")
+child.print_name()
+child.print_cartoon("Gumball")
